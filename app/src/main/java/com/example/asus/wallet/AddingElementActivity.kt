21379531959a1fddb47bef45f590c2aea017d041
DESKTOP-S9060EB\ASUS <xinia214@gmail.com>
2017-12-25 00:53:22 +0600

    Project Initialization

---
+++ app/src/main/java/com/example/asus/wallet/AddingElementActivity.kt
@@ -0,0 +1,12 @@
+package com.example.asus.wallet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddingElementActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adding_element_layout)
+    }
+}
