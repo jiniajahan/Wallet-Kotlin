0656e145bfc87cdbed903efc9c47c8f836ad3688
DESKTOP-S9060EB\ASUS <xinia214@gmail.com>
2017-12-25 13:12:00 +0600

    All Intent started

--- app/src/main/java/com/example/asus/wallet/MainActivity.kt
+++ app/src/main/java/com/example/asus/wallet/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.asus.wallet
 
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toolbar
 
 import kotlinx.android.synthetic.main.activity_main_layout.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,22 +29,30 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-        
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val addNew = findViewById<Button>(R.id.addNew) as Button
+        addNew.setOnClickListener {
+            val intent = Intent(applicationContext, AddingElementActivity::class.java)
+            startActivity(intent)
+            Log.e("AddingElementActivity", "started")
         }
+
+        val showAllItem = findViewById<Button>(R.id.showAllItem) as Button
+        showAllItem.setOnClickListener{
+            val intent = Intent(applicationContext,DataListActivity::class.java)
+            startActivity(intent)
+            Log.e("DataListActivity","started")
+        }
+
+        val userHistory = findViewById<Button>(R.id.userHistory) as Button
+        userHistory.setOnClickListener{
+            val intent = Intent(applicationContext,AddingElementActivity::class.java)
+            startActivity(intent)
+            Log.e("AddingElementActivity","started")
+        }
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/asus/wallet/UserHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.asus.wallet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_history_layout)
+    }
+}
